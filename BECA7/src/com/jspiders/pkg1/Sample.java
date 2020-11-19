c7ba64430f29c1961f69848cc2009633134ffdca
Madhu <Madhu@192.168.0.31>
2020-11-19 10:38:48 +0530

    Packages updated

--- BECA7/src/com/jspiders/pkg1/Sample.java
+++ BECA7/src/com/jspiders/pkg1/Sample.java
@@ -1,5 +1,16 @@
 package com.jspiders.pkg1;
 
-public class Sample {
-
+public class Sample 
+{
+    static int v1 = 100;
+    double v2 = 2.45;
+    
+    public static void count()
+    {
+    	System.out.println("static-count() of pkg1-Sample class");
+    }
+    public void view()
+    {
+    	System.out.println("view() of pkg1-Sample class");
+    }
 }
