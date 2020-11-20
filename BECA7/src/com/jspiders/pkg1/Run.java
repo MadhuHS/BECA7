106508fccab32bc6654e5b258235c2bfd4386980
Madhu <Madhu@192.168.0.31>
2020-11-20 09:26:38 +0530

    Access Specifier programs added and notes updated.

--- BECA7/src/com/jspiders/pkg1/Run.java
+++ BECA7/src/com/jspiders/pkg1/Run.java
@@ -1,23 +1,21 @@
-package com.jspiders.pkg2;
+package com.jspiders.pkg1;
 
-import com.jspiders.pkg1.Sample;
-
-public class Demo extends Sample 
+public class Run 
 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Program starts...");
-
+        
 		Sample s1 = new Sample();
 		System.out.println(s1.v1);
 		s1.view();
-
-		Demo d1 = new Demo();
-		System.out.println(d1.v2);
-		d1.view2();
 		
-	
-
+		System.out.println(s1.v2);
+		s1.view2();
+		
+		System.out.println(s1.v3);
+		s1.view3();
+		
 		System.out.println("Program ends...");
 	}
 }
--- BECA7/src/com/jspiders/pkg1/Sample.java
+++ BECA7/src/com/jspiders/pkg1/Sample.java
@@ -2,15 +2,28 @@ package com.jspiders.pkg1;
 
 public class Sample 
 {
-    static int v1 = 100;
-    double v2 = 2.45;
+    public    int    v1 = 100;
+    protected double v2 = 4.12;
+              long   v3 = 3910930139022l;
+    private   String v4 = "java";
     
-    public static void count()
-    {
-    	System.out.println("static-count() of pkg1-Sample class");
-    }
     public void view()
     {
-    	System.out.println("view() of pkg1-Sample class");
+    	System.out.println("public-view() of Sample class");
+    }
+    
+    protected void view2()
+    {
+    	System.out.println("protected-view2() of Sample class");
+    }
+    
+    void view3()
+    {
+    	System.out.println("pkg-level-view3() of Sample class");
+    }
+    
+    private void view4()
+    {
+    	System.out.println("private-view4() of Sample class");
     }
 }
