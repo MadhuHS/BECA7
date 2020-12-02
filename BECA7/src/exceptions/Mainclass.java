2733d06521b2a6ae1b03d0d2f0ecc4b97a89decb
Madhu <Madhu@192.168.100.101>
2020-12-02 15:35:45 +0530

    Exceptions updated

--- BECA7/src/exceptions/Mainclass.java
+++ BECA7/src/exceptions/Mainclass.java
@@ -1,28 +1,24 @@
 package exceptions;
 
-public class Mainclass 
-{
-	public static void test(int num)
-	{
-	   System.out.println("test() starts...");
-	   
-	   try
-	   {
-	    int res = 50 / num;
-	    System.out.println(res);
-	   }
-	   catch(ArithmeticException ae)
-	   {
-		 System.out.println("INVALID NUM for the DIVISION operation");  
-	   }
-	   
-	   System.out.println("test() ends...");
+public class Mainclass {
+	public static void test(int num) {
+		System.out.println("test() starts...");
+
+		int res = 50 / num;
+		System.out.println(res);
+
+		System.out.println("test() ends...");
 	}
-	
-	public static void main(String[] args) 
-	{
-       System.out.println("Main starts...");
-       test(0);
-       System.out.println("Main ends...");
+
+	public static void main(String[] args) {
+		System.out.println("Main starts...");
+		try 
+		{
+			test(0);
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		System.out.println("Main ends...");
 	}
 }
